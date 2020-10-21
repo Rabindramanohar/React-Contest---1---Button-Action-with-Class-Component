d39029b3289f98d77e645605af184b9fb21b3783
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2020-10-21 21:26:58 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  state = {
+    para : ""
+  }
+
+ addPara = () => {
+    this.setState({
+    para: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+    })
+ }
+
+
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button onClick={this.addPara}>Show Para</button>
+        <p>{this.state.para}</p>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
